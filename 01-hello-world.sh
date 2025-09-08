868c0085f58e274e138a74a8a0f9b093fb0d6529
Mansu2022 <manjunathmouli@gmail.com>
2025-09-09 02:04:00 +0530

    added new file called variable

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 echo "Hello World, I am learning DevOps with AWS"
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Very Good Morning $person1"
